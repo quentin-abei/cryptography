9c932169d6c96e0e1783ca5ceef9b5188637d68e
quentin-abei <quentincrto@gmail.com>
2023-08-17 16:43:08 +0100

    hamming distance

---
+++ set1/breakRepeatXor.js
@@ -0,0 +1,20 @@
+import { stringToBytes } from "./repeatXor.js";
+
+function calculateHammingBytes(string1, string2) {
+    let result1 = stringToBytes(string1);
+    let result2 = stringToBytes(string2);
+    let hammingDistance = 0;
+
+    for (let i=0; i < result1.length; i++) {
+        let xor = result1[i] ^ result2[i];
+
+        while(xor > 0){
+            // add the 1 bits to hammingDistance
+            hammingDistance += xor & 1;
+            // shift the xor bits to the right by 1
+            xor >>= 1;
+        }
+    }
+    console.log(hammingDistance);
+    return hammingDistance;
+}
--- set1/repeatXor.js
+++ set1/repeatXor.js
@@ -20,6 +20,7 @@ function stringToBytes(string) {
         let byte = string.charCodeAt(i % string.length);
         bytes.push(byte);
     }
+    //console.log(bytes);
     return bytes;
 }
 
@@ -39,7 +40,8 @@ function repeatKeyXor(string, key) {
     return result;
 }
 
-let string = "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"
-let key = "ICE"
-repeatKeyXor(string, key);
+export {stringToBytes};
+// let string = "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"
+// let key = "ICE"
+// repeatKeyXor(string, key);
 // 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20690a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
